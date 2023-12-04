983aaf7be51be8c7c7f71b752e55e4bd6de9b668
Karim Sarkulov <karimsarkulov312@gmail.com>
2023-12-04 21:05:18 +0300

    Add all chacters cards

--- cookie run wiki/src/components/templates/characters/characters.jsx
+++ cookie run wiki/src/components/templates/characters/characters.jsx
@@ -1,57 +1,132 @@
 import React, { useState } from "react";
-import "./character.css"; // Подключите файл стилей, если необходимо
-
-const characters = [
-  {name: "Gingerbrave", img: "../src/assets/Cookie_Card/Cookie_gingerbrave_card.png", rarity: "Common", class: "Charge", position: "Front", element: "None",},
-  {name: "Strawberry", img: "../src/assets/Cookie_Card/Cookie_strawberry_card.png", rarity: "Common", class: "Charge", position: "Front", element: "None",},
-  {name: "Wizard", img: "../src/assets/Cookie_Card/Cookie_wizard_card.png", rarity: "Common", class: "Magic", position: "Middle", element: "None",},
-  {name: "Beet", img: "../src/assets/Cookie_Card/Cookie_beet_card.png", rarity: "Common", class: "Ranged", position: "Rear", element: "None",},
-  {name: "Angel", img: "../src/assets/Cookie_Card/Cookie_angel_card.png", rarity: "Common", class: "Healing", position: "Rear", element: "None",},
-  {name: "Ninja", img: "../src/assets/Cookie_Card/Cookie_ninja_card.png", rarity: "Common", class: "Ambush", position: "Middle", element: "None",},
-  {name: "Muscle", img: "../src/assets/Cookie_Card/Cookie_muscle_card.png", rarity: "Common", class: "Charge", position: "Front", element: "None",},
-  {name: "Adventurer", img: "../src/assets/Cookie_Card/Cookie_adventurer_card.png", rarity: "Rare", class: "Charge", position: "Front", element: "None",},
-  {name: "Alchemist", img: "../src/assets/Cookie_Card/Cookie_alchemist_card.png", rarity: "Rare", class: "Charge", position: "Front", element: "None",},
-  {name: "Avocado", img: "../src/assets/Cookie_Card/Cookie_avocado_card.png", rarity: "Rare", class: "Magic", position: "Middle", element: "None",},
-  {name: "Blackberry", img: "../src/assets/Cookie_Card/Cookie_blackberry_card.png", rarity: "Rare", class: "Ranged", position: "Rear", element: "None",},
-  {name: "Cherry", img: "../src/assets/Cookie_Card/Cookie_cherry_card.png", rarity: "Rare", class: "Healing", position: "Rear", element: "None",},
-  {name: "Clover", img: "../src/assets/Cookie_Card/Cookie_clover_card.png", rarity: "Rare", class: "Ambush", position: "Middle", element: "None",},
-  {name: "Custard", img: "../src/assets/Cookie_Card/Cookie_custard_card.png", rarity: "Rare", class: "Charge", position: "Front", element: "None",},
-  {name: "Devil", img: "../src/assets/Cookie_Card/Cookie_devil_card.png", rarity: "Rare", class: "Charge", position: "Front", element: "None",},
-  {name: "Gumball", img: "../src/assets/Cookie_Card/Cookie_gumball_card.png", rarity: "Rare", class: "Magic", position: "Middle", element: "None",},
-  {name: "Knight", img: "../src/assets/Cookie_Card/Cookie_knight_card.png", rarity: "Rare", class: "Ranged", position: "Rear", element: "None",},
-  {name: "Onion", img: "../src/assets/Cookie_Card/Cookie_onion_card.png", rarity: "Rare", class: "Healing", position: "Rear", element: "None",},
-  {name: "Pancake", img: "../src/assets/Cookie_Card/Cookie_pancake_card.png", rarity: "Rare", class: "Ambush", position: "Middle", element: "None",},
-  {name: "Princess", img: "../src/assets/Cookie_Card/Cookie_princess_card.png", rarity: "Rare", class: "Charge", position: "Front", element: "None",},
-];
+import "./character.css"; 
+import characters from "./Data_ch";
 
 const Character = () => {
   const [showFilters, setShowFilters] = useState(false);
+  const [selectedElement, setSelectedElement] = useState(null);
+  const [selectedClass, setSelectedClass] = useState(null);
+  const [selectedPosition, setSelectedPosition] = useState(null);
+  const [selectedRarity, setSelectedRarity] = useState(null);
 
   const toggleFilters = () => {
     setShowFilters(!showFilters);
   };
 
+  const handleElementChange = (element) => {
+    setSelectedElement(element);
+  };
+
+  const handleClassChange = (characterClass) => {
+    setSelectedClass(characterClass);
+  };
+
+  const handlePositionChange = (position) => {
+    setSelectedPosition(position);
+  };
+
+  const handleRarityChange = (rarity) => {
+    setSelectedRarity(rarity);
+  };
+
+  const resetFilters = () => {
+    setSelectedElement(null);
+    setSelectedClass(null);
+    setSelectedPosition(null);
+    setSelectedRarity(null);
+  };
+
+  const filteredCharacters = characters
+    .filter((character) =>
+      selectedElement ? character.element === selectedElement : true
+    )
+    .filter((character) =>
+      selectedClass ? character.class === selectedClass : true
+    )
+    .filter((character) =>
+      selectedPosition ? character.position === selectedPosition : true
+    )
+    .filter((character) =>
+      selectedRarity ? character.rarity === selectedRarity : true
+    );
+
   return (
     <div className="content">
       <div className="Sort_head" onClick={toggleFilters}>
-        <img className="Sort_Icon" src="icons8-filter-100.svg" alt="Filter" />
+        <img className="Sort_Icon" src="../src/assets/Other/Sort_Icon.png" alt="Filter" />
       </div>
 
       {showFilters && (
         <div className="sort" id="myDIV">
-          {/* Ваш код для фильтров */}
+          <div>
+            <label>Элемент:</label>
+            <select onChange={(e) => handleElementChange(e.target.value)}>
+              <option value="">All</option>
+              <option value="None">None</option>
+              <option value="fire">Fire</option>
+              <option value="poison">Poison</option>
+              <option value="light">Light</option>
+              <option value="water">Water</option>
+              <option value="earth">Earth</option>
+              <option value="ice">Ice</option>
+            </select>
+          </div>
+          <div>
+            <label>Класс:</label>
+            <select onChange={(e) => handleClassChange(e.target.value)}>
+              <option value="">All</option>
+              <option value="Ambush">Ambush</option>
+              <option value="Bomber">Bomber</option>
+              <option value="BTS">BTS</option>
+              <option value="Charge">Charge</option>
+              <option value="Defense">Defense</option>
+              <option value="Healing">Healing</option>
+              <option value="Magic">Magic</option>
+              <option value="Ranged">Ranged</option>
+              <option value="Support">Support</option>
+            </select>
+          </div>
+          <div>
+            <label>Позиция:</label>
+            <select onChange={(e) => handlePositionChange(e.target.value)}>
+              <option value="">All</option>
+              <option value="Front">Front</option>
+              <option value="Middle">Middle</option>
+              <option value="Rear">Rear</option>
+              {/* Добавьте остальные позиции */}
+            </select>
+          </div>
+          <div>
+            <label>Редкость:</label>
+            <select onChange={(e) => handleRarityChange(e.target.value)}>
+              <option value="">All</option>
+              <option value="Common">Common</option>
+              <option value="Rare">Rare</option>
+              <option value="Special">Special</option>
+              <option value="Epic">Epic</option>
+              <option value="Super_epic">Super epic</option>
+              <option value="Legendary">Legendary</option>
+              <option value="Dragon">Dragon</option>
+              <option value="Ancient">Ancient</option>
+              <option value="Guest">Guest</option>
+              {/* Добавьте остальные редкости */}
+            </select>
+          </div>
+          <button onClick={resetFilters}>Сбросить фильтры</button>
         </div>
       )}
 
       <ul className="characters">
-        {characters.map((character, index) => (
+        {filteredCharacters.map((character, index) => (
           <li className="cookie" key={index}>
-            <a href={`/${character.name.toLowerCase().replace(" ", "_")}.jsx`}>
+            <a href={`/${character.name.toLowerCase().replace(" ", "_")}`}>
               <img src={character.img} alt={character.name} />
             </a>
           </li>
         ))}
       </ul>
+
+  
     </div>
   );
 };
--- cookie run wiki/src/components/templates/header/header.jsx
+++ cookie run wiki/src/components/templates/header/header.jsx
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './header.module.css';
-import CloseNav from './CloseNav.jsx';
 
-const openNav = () => {
-    setNavWidth('100%');
+function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const openNav = () => {
+    setIsNavOpen(true);
   };
 
   const closeNav = () => {
-    setNavWidth('0%');
+    setIsNavOpen(false);
   };
 
-function Header() {
-    return (
-        <header>
-        <div id="myNav" className="overlay">
-            <a href="javascript:void(0)" className="closebtn" onclick="CloseNav()">&times;</a>
-            <div className="overlay-content">
-                <a href="Menu.html"><img className="MainMenu" src="../src/assets/MainLogo.png" alt="image 1"></img></a>
-                <a href="Menu.html">Menu</a>
-                <a href="main.html">Characters</a>
-                <a href="Map.html">Map</a>
-                <a href="Profile.html">Profile</a>
-            </div>
+  return (
+    <header>
+      {isNavOpen && (
+        <div className="overlay">
+          <a href="#!" className="closebtn" onClick={closeNav}>&times;</a>
+          <div className="overlay-content">
+            <a href="Menu.html"><img className="MainMenu" src="../src/assets/MainLogo.png" alt="image 1" /></a>
+            <Link to="Menu.html" onClick={closeNav}>Menu</Link>
+            <Link to="main.html" onClick={closeNav}>Characters</Link>
+            <Link to="Map.html" onClick={closeNav}>Map</Link>
+            <Link to="Profile.html" onClick={closeNav}>Profile</Link>
+          </div>
         </div>
-        <span className="Menu" onclick="OpenNav()">&#9776;</span>
-        <a href="Menu.html"><img src="../src/assets/Logo.png" alt="image 1"></img></a>
-        </header>
-    );
+      )}
+
+      <span className="Menu" style={{ fontSize: "45px", cursor: "pointer", color: "white"}} onClick={openNav}>☰</span>
+      <Link to="Menu.html"><img src="../src/assets/Logo.png" alt="image 1" /></Link>
+    </header>
+  );
 }
 
 export default Header;
