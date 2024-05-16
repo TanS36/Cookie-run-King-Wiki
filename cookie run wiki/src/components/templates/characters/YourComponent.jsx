860348d0d08c4269d8d361f28a05c9976c804a9e
Karim Sarkulov <karimsarkulov312@gmail.com>
2024-05-16 19:08:05 +0300

    need fix this

--- cookie run wiki/src/components/templates/characters/YourComponent.jsx
+++ cookie run wiki/src/components/templates/characters/YourComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './YourComponent.module.sass';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -6,31 +6,7 @@ import { auth } from '../../../../firebase.js';
 
 const YourComponent = ({ character, isFavorite }) => {
   const [isHovered, setIsHovered] = useState(false);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [user, loading, error] = useAuthState(auth);
-  const imageRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setIsLoaded(true);
-          observer.unobserve(imageRef.current);
-        }
-      },
-      { threshold: 0.5 }
-    );
-
-    if (imageRef.current) {
-      observer.observe(imageRef.current);
-    }
-
-    return () => {
-      if (imageRef.current) {
-        observer.unobserve(imageRef.current);
-      }
-    };
-  }, []);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -41,20 +17,14 @@ const YourComponent = ({ character, isFavorite }) => {
   };
 
   return (
-    <div className={`${styles.image_container} ${isHovered? styles.hovered : ''}`}
+    <div className={`${styles.image_container} ${isHovered ? styles.hovered : ''}`}
          onMouseEnter={handleMouseEnter}
          onMouseLeave={handleMouseLeave}>
-      {user &&!loading && (
-        <div className={`${styles.star_icon} ${isFavorite? styles.favorite : ''}`}></div>
+      {user && !loading && (
+        <div className={`${styles.star_icon} ${isFavorite ? styles.favorite : ''}`}></div>
       )}
       <Link to={`/characters/${character.name}`} className={styles.link_style}>
-        <img
-          ref={imageRef}
-          src={isLoaded? character.img : ''}
-          alt={character.name}
-          className={styles.character_image}
-          style={{ visibility: isLoaded? 'visible' : 'hidden' }}
-        />
+        <img src={character.img} alt={character.name} className={styles.character_image} />
         {character.candy && (
           <img src={character.candy_img_3} alt="Candy" className={styles.candy_image} />
         )}
